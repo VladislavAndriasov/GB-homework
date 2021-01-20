04378322e42813c90d7d76fba240eb03177b1818
VladislavAndriasov <77287771+VladislavAndriasov@users.noreply.github.com>
2021-01-21 01:27:40 +0300

    Update Task4.py

--- Task4.py
+++ Task4.py
@@ -7,7 +7,11 @@ print(result)
 
 
 def my_func_2(x, y):
-    return 1 / (x * y)
+    z = x
+    while y < -1:
+        z = z * x
+        y = y + 1
+    return 1 / z
 
 
 result = my_func(x=int(input('Enter X: ')), y=int(input('Enter Y: ')))
