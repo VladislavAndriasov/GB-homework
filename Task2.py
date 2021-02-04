0f020ae6ae5fb698a56c307bfa14be3555d8d386
VladislavAndriasov <77287771+VladislavAndriasov@users.noreply.github.com>
2021-02-04 19:54:28 +0300

    lesson 6 homework

--- Task2.py
+++ Task2.py
@@ -1,8 +1,17 @@
-my_list = input('Enter elements in list separated by a space : ').split()
-element = len(my_list)
-i = 0
-while i < element:
-    temp = my_list.pop(i)
-    my_list.insert(i + 1, temp)
-    i += 2
-print(my_list)
+class Road:
+    def __init__(self, _length, _width):
+        self._length = _length
+        self._width = _width
+
+    def mass(self):
+        return self._length * self._width
+
+
+class MassCount(Road):
+    def __init__(self, _length, _width, volume):
+        super().__init__(_length, _width)
+        self.volume = volume
+
+
+r = MassCount(25, 10000, 125)
+print(r.mass())
